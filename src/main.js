72a44c2bd7f87ad2189449efb27ee2583a0cba88
junrong Li <junrongli@junrongs-MBP.hub>
2021-04-14 10:05:01 +1000

    main

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
+import Phaser from "phaser";
+
 import singleSpikeImg from "./assets/single-spike.png";
 import spikeBoardImg from "./assets/spike-board.png";
 import wheelImg from "./assets/wheel.png";
 import normalBoardImg from "./assets/board.png";
 import playerImg from "./assets/player.png";
-import "./style.css";
-import Phaser from "phaser";
+import bounceBoardImg from "./assets/bounce-Board.png";
+import restartButtonImg from './assets/btn.png';
 
-let gameScene = new Phaser.Scene('Game');
+import "./style.css";
+
+let gameScene = new Phaser.Scene('MainScene');
 
 gameScene.init = function() {
-    this.platformComponents = ['wheel-board', 'normal-board', 'spike-board'];
+    this.platformComponents = ['wheel-board', 'normal-board', 'spike-board', 'bounce-board'];
+    this.score = 0;
+    this.health = 3;
+
 };
 
 gameScene.preload = function() {
@@ -17,49 +24,87 @@ gameScene.preload = function() {
     this.load.image('spike-board', spikeBoardImg);
     this.load.image('wheel-board', wheelImg);
     this.load.image('normal-board', normalBoardImg);
+    this.load.image('bounce-board', bounceBoardImg);
+
     this.load.image('player', playerImg);
 };
 gameScene.createBoard = function(x, y, boardType) {
-
-    const board = this.physics.add.sprite(x, y, boardType).setOrigin(0, 0);
+    let board;
+    if (boardType == 'bounce-board') {
+        board = this.physics.add.sprite(x, y, boardType).setOrigin(0, 0).setScale(2);
+    }else{
+        board = this.physics.add.sprite(x, y, boardType).setOrigin(0, 0);
+    }
     board.body.allowGravity = false;
     board.setVelocityY(-80);
     board.setImmovable(true);
     board.bType = boardType;
+    if(boardType=='wheel-board'){
+        if (Phaser.Math.Between(0,1)==1) {
+            board.dir = 1
+        } else {
+            board.dir = -1
+        }
+    }
+    
     return board;
 };
 gameScene.create = function() {
-    this.add.tileSprite(0, 0, 17 * 14, 19, 'single-spike').setOrigin(0, 0).setScale(1.5);
+    this.playerDeath = this.add.tileSprite(0, 0, 17 * 14, 19, 'single-spike').setOrigin(0, 0).setScale(1.5);
+    this.physics.add.existing(this.playerDeath);
+    this.playerDeath.body.allowGravity = false;
+    //this.playerDeath.setImmovable(true);
     this.platforms = this.add.group();
     const aBoard = this.createBoard(250, 300, 'normal-board');
     this.platforms.add(aBoard);
-    this.player = this.physics.add.sprite(300,300-80,'player').setScale(0.5);
-    
+    this.player = this.physics.add.sprite(300, 300 - 80, 'player').setOrigin(0).setScale(0.5);
+    this.player.body.collideWorldBounds=true;
     this.spawnEvent = this.time.addEvent({
         delay: 1000,
         loop: true,
         callbackScope: this,
         callback: function() {
-            let whichBoard = this.platformComponents[Phaser.Math.Between(0, this.platformComponents.length-1)];
+            let whichBoard = this.platformComponents[Phaser.Math.Between(0, this.platformComponents.length - 1)];
             const x = Phaser.Math.Between(30, config.width - 100);
-            const b = this.createBoard(x,600, whichBoard);
+            const b = this.createBoard(x, 600, whichBoard);
             this.platforms.add(b);
+            this.score++;
         }
     });
+    
+    this.physics.add.collider(this.player, this.playerDeath, function() {
+        gameScene.health=0;
+    });
 
-    this.physics.add.collider(this.player, this.platforms, function(player, board){
-    	let now = new Date();
-    	let allowReduceHealth = false;
-    	if (!player.spikeCollideTime){
-    		allowReduceHealth = true;
-    	}else if(now - player.spikeCollideTime>1000){
-    		allowReduceHealth = true;
-    	}
+    this.physics.add.collider(this.player, this.platforms, (player, board) => {
+        let now = new Date();
+        let allowReduceHealth = false;
+        if (!player.spikeCollideTime) {
+            allowReduceHealth = true;
+        } else if (now - player.spikeCollideTime > 1000) {
+            allowReduceHealth = true;
+        }
 
-    	if(board.bType=='spike-board' && allowReduceHealth){
-    		player.spikeCollideTime = new Date();
-    		player.health--;
-    	}
+        if (board.bType == 'spike-board' && allowReduceHealth) {
+            player.spikeCollideTime = new Date();
+            gameScene.health--;
+        }
+        if (board.bType == 'bounce-board') {
+            player.setVelocityY(-600);
+        }
+
+        if (board.bType == 'wheel-board') {
+            
+            // = Phaser.Math.Between(0,1)
+            player.x-=2*board.dir;//.setVelocityX(80);
+        }else{
+
+        }
+
+        //if(this.player.health == 0){
+        //allowReduceHealth = false;
+
+        //};
     });
 
     this.cursorKeys = this.input.keyboard.createCursorKeys();
@@ -74,33 +119,78 @@ gameScene.create = function() {
         //     }
         // }
     }, this);
-    this.player.health = 3;
-    this.healthLabel = this.add.text(0,20, 'Health:');
-    this.scoreLabel = this.add.text(0,40, 'Score:');
-    
-};
-gameScene.checkGameOver = function(){
-	if(this.player.health<=0){
-		this.gameOver = true;
-		// this.gameOverLabel.visible = true;
-	}
-};
-gameScene.update = function() {
-    if (this.cursorKeys.right.isDown) {
-    	this.player.setVelocityX(200);
-    }else if (this.cursorKeys.left.isDown) {
-    	this.player.setVelocityX(-200);
-    }else{
-    	this.player.setVelocityX(0);
-    }
-    this.healthLabel.setText(`Health: ${this.player.health}`);
+
+    this.healthLabel = this.add.text(0, 20, 'Health:');
+    this.scoreLabel = this.add.text(0, 40, 'Score:');
+
 };
 
+
+gameScene.checkGameOver = function() {
+    if (this.health <= 0||this.player.y >= 560) {
+        this.scene.launch('GameOverScene');
+        this.scene.pause();
+    }
+    console.log(this.player);
+
+};
+gameScene.update = function() {
+    // if (this.cursorKeys.space.isDown) {
+    //     if (!gameOver) {
+    //         if (!paused) {
+    //             this.scene.pause();
+    //             paused = true;
+    //         } else {
+    //             this.scene.resume();
+    //         }
+    //     }
+
+    // };score
+    // if (this.score > this.highScore) {
+    //     this.highScore = this.score;
+    // }
+    this.healthLabel.setText(`Health: ${this.health}`);
+    this.scoreLabel.setText(`score: ${this.score}`);
+    this.checkGameOver();
+    
+    if (this.cursorKeys.right.isDown) {
+        this.player.x+=4;//.setVelocityX(200);
+    } else if (this.cursorKeys.left.isDown) {
+        this.player.x-=4;
+        // this.player.setVelocityX(-200);
+    }
+    //  else {
+    //     this.player.setVelocityX(0);
+    // }
+    if (this.cursorKeys.up.isDown) {
+        this.player.setVelocityY(-600);
+    }
+    
+};
+
+let gameOverScene = new Phaser.Scene('GameOverScene');
+gameOverScene.preload=function(){
+    this.load.image('rst.btn', restartButtonImg);
+};
+gameOverScene.create = function(){
+
+    this.add.text(10, 200, 'Game Over!',{
+            fontSize: '60px'
+    });
+    this.add.text(140,400, `Score: ${gameScene.score}`)
+    const rst = this.add.sprite(180,300,'rst.btn').setScale(0.25);
+    rst.setInteractive();
+    rst.on('pointerdown', function(){
+        // this.scene.resume("MainScene");
+        this.scene.get("MainScene").scene.restart();
+        this.scene.stop();
+    }, this);
+}
 let config = {
     type: Phaser.AUTO,
     width: 360,
     height: 600,
-    scene: gameScene,
+    scene: [gameScene, gameOverScene],
     title: '100',
     pixelArt: false,
     physics: {
@@ -109,11 +199,8 @@ let config = {
             gravity: {
                 y: 1000
             },
-            debug: true
+            debug: false
         }
     }
 };
-
 let game = new Phaser.Game(config);
-
-// let gameOverScene = new Phaser.Scene("Game");
